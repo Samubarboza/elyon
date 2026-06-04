efd860f80f0d27a7619767df0ce9cfb5203f3a94
Samuel Barboza <samu_junior95@hotmail.com>
2026-06-04 20:41:41 -0300

    added firts models to module users, create firts migrations and register in admin

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -1,7 +1,24 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 
-from apps.users.models import User
+from apps.users.models import Company, Plan, User
 
 
 admin.site.register(User, UserAdmin)
+
+
+@admin.register(Plan)
+class PlanAdmin(admin.ModelAdmin):
+    list_display = ('name', 'max_users')
+
+
+@admin.register(Company)
+class CompanyAdmin(admin.ModelAdmin):
+    list_display = ('name', 'plan')
+
+    # La empresa es unica: no se crea otra ni se elimina
+    def has_add_permission(self, request):
+        return not Company.objects.exists()
+
+    def has_delete_permission(self, request, company=None):
+        return False
