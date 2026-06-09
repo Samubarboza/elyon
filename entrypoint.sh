dfcaa531a8ed1d3a9fd364ca7025874204168a21
Samuel Barboza <samu_junior95@hotmail.com>
2026-06-09 20:58:31 -0300

    se agrego collectstatic al script entrypoint para servir los estaticos al levantar los contenedores

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 set -e
 
-# Prepara la base y los grupos antes de arrancar el servidor
+# Prepara la base, los grupos y los estaticos antes de arrancar el servidor
 python manage.py migrate --noinput
 python manage.py setup_groups
+python manage.py collectstatic --noinput
 
 exec "$@"
